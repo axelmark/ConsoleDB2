13939249cbf57b5c5e73713423f56c1b1db5521d
axelmark <axelmark@outlook.com>
2017-02-09 18:42:53 +0700

    1

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace ConsoleDB2
+{
+    static class Program
+    {
+        static void Main()
+        {
+            SqLQueries sqLQueries = new SqLQueries();
+            string str = null;
+
+            Console.WriteLine("Welcome!");
+            while (str != "")
+            {
+                String _name = null, _age = null, _gen = null, _id = null;
+                str = Console.ReadLine();
+
+                switch (str)
+                {
+                    case "select":
+                        sqLQueries.SelectFromTable();
+                        break;
+
+                    case "insert":
+                        FillInTheRequiredFieldsInsert(_name, _age, _gen);
+                        sqLQueries.InsertIntoTable(_name, Convert.ToInt32(_age), _gen);
+                        break;
+
+                    case "update":
+                        FillInTheRequiredFieldsUpdate(_name, _id);
+                        sqLQueries.UpdateTable(_name, Convert.ToInt32(_id));
+                        break;
+
+                    case "delete":
+                        FillInTheRequiredFieldsDelete(_id);
+                        sqLQueries.DeleteFromTable(Convert.ToInt32(_id));
+                        break;
+
+                    default:
+                        Console.WriteLine("Команда не найдена!");
+                        break;
+                }
+            }
+        }
+
+        private static void FillInTheRequiredFieldsInsert(String _name, String _age, String _gen)
+        {
+            Console.Write("Введите имя: ");
+            _name = Console.ReadLine();
+            Console.Write("Введите возраст:  ");
+            _age = Console.ReadLine();
+            Console.Write("Введите пол муж/жен: ");
+            _gen = Console.ReadLine();
+        }
+
+        private static void FillInTheRequiredFieldsUpdate(String _name, String _id)
+        {
+            Console.Write("Введите номер строки в которой, желаете изменить имя: ");
+            _id = Console.ReadLine();
+            Console.Write("Введите новое имя: ");
+            _name = Console.ReadLine();
+        }
+
+        private static void FillInTheRequiredFieldsDelete(String _id)
+        {
+            Console.Write("Введите номер строки для удаления: ");
+            _id = Console.ReadLine();
+        }
+    }
+}
---
+++ SQLqueries.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ConsoleDB2
+{
+    class SqLQueries
+    {
+        private static readonly string ConString =
+            "Data Source=APPLE;Initial Catalog=Basic;Integrated Security=True; Pooling=true";
+
+        private void TransmissionOfCommandToDataBase(string requestsString)
+        {
+            using (SqlConnection connection = new SqlConnection(ConString))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlTransaction transaction = connection.BeginTransaction();
+                    SqlCommand command = new SqlCommand(requestsString, connection) {Transaction = transaction};
+                    int result = command.ExecuteNonQuery();
+
+                    if (result > 0)
+                    {
+                        transaction.Commit();
+                        ShowMessageInfo("выполнено успешно");
+                    }
+                    else
+                    {
+                        transaction.Rollback();
+                        ShowMessageInfo("ошибка при выполнении");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            SelectFromTable();
+        }
+
+        public void SelectFromTable()
+        {
+            using (SqlConnection connection = new SqlConnection(ConString))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = connection.CreateCommand();
+                    command.CommandText = "SELECT * FROM [new_table]";
+                    SqlDataReader dr = command.ExecuteReader();
+
+                    if (dr.HasRows)
+                    {
+                        while (dr.Read())
+                        {
+                            Console.WriteLine($"{dr[0]} {dr[1]} {dr[2]}");
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+
+        public void InsertIntoTable(string name, int age, string gen)
+        {
+            TransmissionOfCommandToDataBase("INSERT INTO [new_table] ([name], [age], [gen]) VALUES ('" + name + "','" +
+                                            age + "','" + gen + "')");
+        }
+
+        public void UpdateTable(string name, int id)
+        {
+            TransmissionOfCommandToDataBase("UPDATE [new_table] SET [name] = '" + name + "' WHERE [id]='" + id + "'");
+        }
+
+        public void DeleteFromTable(int id)
+        {
+            TransmissionOfCommandToDataBase("DELETE FROM [new_table] WHERE [id] = '" + id + "'");
+        }
+
+        private void ShowMessageInfo(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
